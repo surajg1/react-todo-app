0ac0778c84a78a007d47a9eee68f707699633f85
surajg1 <surajgholap1234@gmail.com>
2019-09-18 08:38:21 +0530

    Second chages

---
+++ src/App.jsx
@@ -0,0 +1,102 @@
+import React from 'react';
+import logo from './nonja.png';
+import './App.css';
+
+class App extends React.Component{
+    constructor(props){
+        super(props);
+        this.state ={
+            newItem:"",
+            list :[]
+        }
+    }   
+
+    addItem(todoValue){
+        if(todoValue!==""){
+            const newItem ={
+                id: Date.now(),
+                value:todoValue,
+                isDone:false    
+            };
+
+            const list= [...this.state.list];
+            list.push(newItem);
+
+            this.setState({
+                list,
+                newItem:""
+            });
+        }
+    }
+
+    deleteItem(id){
+        const list=[...this.state.list];
+        const updatedlist = list.filter(item=>item.id !== id);
+        this.setState({list:updatedlist})
+    }
+
+    updateInput(input){
+        this.setState({newItem:input})
+    }
+    render(){
+           return(
+            <div>
+                <img src={logo} width="100" height="100" className="logo"/>
+                <h1 className ="app-title">Suraj ToDo App</h1>
+
+                <div className="container">
+                        Add An item...<br/><br/>
+                        <div id="namer">
+                            <div id="namer-input">
+                    <input type="text" className="input-text" placeholder="write ToDo"
+                    required value={this.state.newItem}
+                    onChange={e => this.updateInput(e.target.value)}/>
+                        <br/><br/>
+                    <button     
+                    onClick={()=>this.addItem(this.state.newItem)}
+                    disabled={!this.state.newItem.length}
+                    className="add-btn"
+                    >
+                        Add ToDo</button>  
+                        </div>
+                        </div>
+                        <br/><br/><br/>
+                        <div className="list">
+                            <br/>
+                            <ul>
+                                {this.state.list.map(item =>{
+                                    return(
+                                      
+                                        <  li key={item.id}>
+                                             
+                                           <input type="checkbox" 
+                                           name="idDone"
+                                           checked={item.isDone}
+                                           onChange={()=>{}}
+                                           />
+                                           {item.value}
+                                           <button 
+                                           className="btn"
+                                           onClick={()=>this.deleteItem(item.id)}>Delete</button>
+                                            
+                                        </li>
+                                        
+                                    );
+                                })}
+                                
+                                <li><br/>
+                                    <input type="checkbox" name="" id=""/>
+                                Learn BootStrap
+                                <button className="btn" >Delete</button>
+                                </li>
+                                </ul>
+                                </div>                                                                              
+                </div>
+            </div>
+           );
+
+           
+       }
+}
+
+export default App
